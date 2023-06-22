d895086b4571228dd97204405fd2ca7d40270a0a
Yael Badillo <yael.badillos@gmail.com>
2023-06-22 09:11:06 -0600

    Refactor: move models and repositories within database dir

--- apps/backend/src/typedi.config.ts
+++ apps/backend/src/typedi.config.ts
@@ -1,7 +1,7 @@
 import Container from 'typedi'
 
 import { WinstonLogger } from './utils/logger'
-import { User } from './models'
+import { User } from './database/models'
 
 const winstonLogger = Container.get<WinstonLogger>(WinstonLogger)
 Container.set('winston.logger', winstonLogger)
