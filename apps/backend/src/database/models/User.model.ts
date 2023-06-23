28ea79770327be92d9d9ea72513c8f3774cacd4b
Yael Badillo <yael.badillos@gmail.com>
2023-06-23 00:19:38 -0600

    Refactor: set user name to unique

--- apps/backend/src/database/models/User.model.ts
+++ apps/backend/src/database/models/User.model.ts
@@ -1,4 +1,4 @@
-import { Document, Schema, model } from 'mongoose'
+import { Schema, model } from 'mongoose'
 
 export interface IUser {
   name: string
@@ -8,7 +8,7 @@ export interface IUser {
 }
 
 const userSchema = new Schema<IUser>({
-  name: { type: String, required: true, max: 20 },
+  name: { type: String, required: true, max: 20, unique: true },
   password: { type: String, required: true, max: 30 },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
