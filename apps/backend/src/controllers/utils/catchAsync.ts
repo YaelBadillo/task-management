18cbd0bdd0df7d6224510a8eea264582d7133ae9
Yael Badillo <yael.badillos@gmail.com>
2023-06-24 14:36:01 -0600

    Refactor: remove console log from catch async function

--- apps/backend/src/controllers/utils/catchAsync.ts
+++ apps/backend/src/controllers/utils/catchAsync.ts
@@ -10,8 +10,6 @@ export const catchAsync =
   (handler: AsyncRequestHandler) =>
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      console.log('hola')
-
       return await handler(req, res, next)
     } catch (error) {
       return next(error)
