b94fc1fafc970404aae8064b305482fb8b76d500
Yael Badillo <yael.badillos@gmail.com>
2023-06-25 12:12:26 -0600

    Refactor register user dto

--- apps/backend/src/shared/dtos/RegisterUser.dto.ts
+++ apps/backend/src/shared/dtos/RegisterUser.dto.ts
@@ -1,25 +1,4 @@
 export class RegisterUserDto {
-  private _name: string
-  private _password: string
-
-  constructor() {
-    this._name = ''
-    this._password = ''
-  }
-
-  public get name(): string {
-    return this._name
-  }
-
-  public set name(value: string) {
-    this._name = value
-  }
-
-  public get password(): string {
-    return this._password
-  }
-
-  public set password(value: string) {
-    this._password = value
-  }
+  name: string
+  password: string
 }
