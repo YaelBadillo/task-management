594a500256eea25901a07072a013b584d8870031
Yael Badillo <yael.badillos@gmail.com>
2023-06-26 16:29:22 -0600

    Feat: add react router dom and basic routes

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,10 +1,33 @@
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from 'react-router-dom'
+
+import { Home } from '@pages/home'
+import { Signup } from '@pages/signup'
+import { Login } from '@pages/login'
+import { AuthContainer, RootContainer } from '@layouts'
+
 function App() {
   return (
-    <>
-      <button className="btn-primary btn">Button</button>
-      <button className="btn-secondary btn">Button</button>
-      <button className="btn-neutral btn">Button</button>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<RootContainer />}>
+          <Route index element={<Home />} />
+
+          <Route path="auth" element={<AuthContainer />}>
+            <Route index element={<Navigate to="/auth/login" />} />
+
+            <Route path="sign-up" element={<Signup />} />
+            <Route path="login" element={<Login />} />
+          </Route>
+        </Route>
+      </Routes>
+
+      <Route path="*" element={<Navigate to="/" />} />
+    </Router>
   )
 }
 
