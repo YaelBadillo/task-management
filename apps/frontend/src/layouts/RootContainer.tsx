cbef4c74c8a8cffe6663a0ad53b199ce10c5df63
Yael Badillo <yael.badillos@gmail.com>
2023-06-26 21:11:19 -0600

    Feat: add navbar

--- apps/frontend/src/layouts/RootContainer.tsx
+++ apps/frontend/src/layouts/RootContainer.tsx
@@ -1,10 +1,30 @@
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
 export const RootContainer = () => {
   return (
-    <div>
-      RootContainer
-      <Outlet />
-    </div>
+    <>
+      <div className="container m-auto">
+        <div className="navbar bg-base-100">
+          <div className="flex-1">
+            <Link to="/" className="btn-ghost btn text-xl normal-case">
+              Task Management App
+            </Link>
+          </div>
+          <div className="flex-none">
+            <ul className="menu menu-horizontal gap-x-1 px-1">
+              <li>
+                <Link to="/auth/sign-up">Sign up</Link>
+              </li>
+              <li>
+                <Link to="/auth/login">Login</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="container m-auto">
+        <Outlet />
+      </div>
+    </>
   )
 }
