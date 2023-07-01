ec012a8485b4b126d4057f66de6f3491b1de8d9a
Yael Badillo <yael.badillos@gmail.com>
2023-07-01 00:21:02 -0600

    Feat: add cors in development environment

--- apps/backend/src/Server.ts
+++ apps/backend/src/Server.ts
@@ -1,20 +1,23 @@
 import http from 'http'
 import path from 'path'
 
-import express, { Express, Request, Response } from 'express'
+import express, { Express } from 'express'
+import { Config } from 'convict'
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
 import Container from 'typedi'
+import cors from 'cors'
 
 import { Logger, WinstonLogger } from '@utils/logger'
 import { registerRoutes } from '@routes'
+import { ConfigSchema } from '@config'
 
 export class Server {
   private readonly express: Express
   private readonly logger: Logger
   private httpServer?: http.Server
 
-  constructor(private readonly port: string) {
+  constructor(private readonly config: Config<ConfigSchema>) {
     this.express = express()
     this.logger = Container.get(WinstonLogger)
 
@@ -24,13 +27,17 @@ export class Server {
       express.static(path.join(__dirname, '../../', 'frontend/dist')),
     )
 
+    if (config.get('env') === 'development') {
+      this.express.use(cors)
+    }
+
     registerRoutes(this.express)
   }
 
   listen(): Promise<void> {
     return new Promise(resolve => {
-      this.httpServer = this.express.listen(this.port, () => {
-        this.logger.info(`Server listening on port: ${this.port}`)
+      this.httpServer = this.express.listen(this.config.get('port'), () => {
+        this.logger.info(`Server listening on port: ${this.config.get('port')}`)
 
         return resolve()
       })
