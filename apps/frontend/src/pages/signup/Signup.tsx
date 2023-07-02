10b42074e558b51afac190a8da483fdb4fe244ce
Yael Badillo <yael.badillos@gmail.com>
2023-07-02 11:44:53 -0600

    Refactor: move Signup page logic to a hook

--- apps/frontend/src/pages/signup/Signup.tsx
+++ apps/frontend/src/pages/signup/Signup.tsx
@@ -1,33 +1,9 @@
-import { useEffect } from 'react'
-
-import { useNavigate } from 'react-router-dom'
-
 import { AuthSwitch, Button, Form, Heading, Input } from '@components/auth-form'
 import { Loading } from '@components/loading'
-import { useForm } from '@hooks'
-import { validate } from '@utils/validate'
-import { SignupSchema } from '@pages/signup/SignupSchema'
+import { useSignup } from '@pages/signup/useSignup'
 
 export const Signup = () => {
-  const { formik, status } = useForm({
-    url: 'http://localhost:3000/api/auth/sign-up',
-    initialValues: {
-      username: '',
-      password: '',
-      passwordConfirm: '',
-    },
-    validate: validate(SignupSchema),
-    initialTouched: {
-      username: false,
-      password: false,
-      passwordConfirm: false,
-    },
-  })
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    if (status === 'success') navigate('/auth/login')
-  }, [status, navigate])
+  const { formik, status } = useSignup()
 
   if (status === 'pending') return <Loading />
 
