ad0ef4fd932f4be799081af8662db9f498deb9af
Yael Badillo <yael.badillos@gmail.com>
2023-07-10 09:44:23 -0600

    Refactor: replace http exception class for http exception two class

--- apps/backend/src/shared/exceptions/HttpException.ts
+++ apps/backend/src/shared/exceptions/HttpException.ts
@@ -1,16 +1,9 @@
 export class HttpException extends Error {
-  readonly status: number
-  readonly message: string
-
-  constructor(status: number, message: string) {
-    super(message)
-    this.status = status
-    this.message = message
-  }
-}
-
-export class HttpExceptionTwo extends Error {
-  constructor(readonly status: number, readonly message: string) {
+  constructor(
+    readonly status: number,
+    readonly message: string,
+    readonly path?: string,
+  ) {
     super(message)
   }
 }
