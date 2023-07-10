09d9b0d51545c2fb69f3230142262c6af1ee2448
Yael Badillo <yael.badillos@gmail.com>
2023-07-10 09:51:21 -0600

    Feat: add path prop to http exception

--- apps/backend/src/shared/exceptions/BadRequestException.ts
+++ apps/backend/src/shared/exceptions/BadRequestException.ts
@@ -2,7 +2,7 @@ import { HttpException } from '@shared/exceptions/HttpException'
 import httpStatus from 'http-status'
 
 export class BadRequestException extends HttpException {
-  constructor(message: string) {
-    super(httpStatus.BAD_REQUEST, message)
+  constructor(message: string, path?: string) {
+    super(httpStatus.BAD_REQUEST, message, path)
   }
 }
