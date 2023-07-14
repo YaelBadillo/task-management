9dc55ead00a63608e9ca3c8b8fddfdf6541070c4
Yael Badillo <yael.badillos@gmail.com>
2023-07-13 21:21:03 -0600

    Refactor: remove http exception class from backend

--- apps/backend/src/shared/exceptions/HttpException.ts
+++
@@ -1,9 +0,0 @@
-export class HttpException extends Error {
-  constructor(
-    readonly status: number,
-    readonly message: string,
-    readonly path?: string,
-  ) {
-    super(message)
-  }
-}
