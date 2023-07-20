66a44ec118e18e9c8b110469520f5b054f3a8a4a
Yael Badillo <yael.badillos@gmail.com>
2023-07-19 19:34:20 -0600

    Refactor: pull apart auth controller interfaces

--- apps/backend/src/shared/interfaces/requests/Requests.interface.ts
+++ apps/backend/src/shared/interfaces/requests/Requests.interface.ts
@@ -1,6 +1,15 @@
 import { Request } from 'express'
 
+import { LogInUserDto, RegisterUserDto } from '@shared/dtos'
 import { IUser } from '@database/models'
+
+export interface RegisterUserRequest extends Request {
+  body: RegisterUserDto
+}
+
+export interface LogInUserRequest extends Request {
+  body: LogInUserDto
+}
 
 export interface RequestWithUser extends Request {
   user?: IUser
