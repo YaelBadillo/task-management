b47727cb239d904281d3eacbf2b4010514184799
Yael Badillo <yael.badillos@gmail.com>
2023-07-26 17:21:58 -0600

    Refactor: add initial value to the use read local storage hook

--- apps/frontend/src/hooks/useReadLocalStorage.ts
+++ apps/frontend/src/hooks/useReadLocalStorage.ts
@@ -1,31 +1,24 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useState } from 'react'
 
 export const useReadLocalStorage = <T>(
   key: string,
-  immediate = true,
 ): [T | undefined, () => void] => {
-  const [storedValue, setValue] = useState<T>()
-
-  const getValue = useCallback(() => {
+  const getValue = useCallback((): T | undefined => {
     try {
       const item = window.localStorage.getItem(key)
-      return item ? (JSON.parse(item) as T) : null
+      return item ? (JSON.parse(item) as T) : undefined
     } catch (error) {
       console.warn(`Error reading localStorage key “${key}”:`, error)
       return undefined
     }
   }, [key])
 
+  const [storedValue, setValue] = useState<T | undefined>(getValue)
+
   const readStoredValue = useCallback(() => {
     const value = getValue()
     if (value) setValue(value)
   }, [getValue])
-
-  useEffect(() => {
-    if (immediate) {
-      readStoredValue()
-    }
-  }, [immediate, readStoredValue])
 
   return [storedValue, readStoredValue]
 }
