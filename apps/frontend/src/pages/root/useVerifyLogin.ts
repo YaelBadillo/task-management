9dc2a10304afc90b62cc35832067f367d39a1885
Yael Badillo <yael.badillos@gmail.com>
2023-07-26 17:22:43 -0600

    Feat: add redirection to the login page if no use profile is found

--- apps/frontend/src/pages/root/useVerifyLogin.ts
+++ apps/frontend/src/pages/root/useVerifyLogin.ts
@@ -1,22 +1,25 @@
 import { useEffect } from 'react'
 
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 import { useReadLocalStorage } from '@hooks'
 
 export const useVerifyLogin = <T>() => {
   const { pathname } = useLocation()
+  const navigate = useNavigate()
 
   const userProfileKey = 'userProfile'
-  const immediate = false
-  const [userProfile, readStorage] = useReadLocalStorage<T>(
-    userProfileKey,
-    immediate,
-  )
+  const [userProfile, readStorage] = useReadLocalStorage<T>(userProfileKey)
 
   useEffect(() => {
     readStorage()
   }, [pathname, readStorage])
+
+  useEffect(() => {
+    if (pathname.includes('dashboard') && userProfile === undefined) {
+      navigate('/auth/login')
+    }
+  }, [pathname, userProfile, navigate])
 
   return userProfile
 }
