a8848569d2b1216d2599acc7997716fed52ab61c
Yael Badillo <yael.badillos@gmail.com>
2023-07-26 19:09:00 -0600

    Refactor: remove use user profile hook

--- apps/frontend/src/pages/login/useLogin.ts
+++ apps/frontend/src/pages/login/useLogin.ts
@@ -1,15 +1,11 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
 import { useNavigate } from 'react-router-dom'
 
 import { useForm } from '@hooks'
-import { useUserProfile } from '@pages/login/useUserProfile'
 
 export const useLogin = () => {
-  const [status, setStatus] = useState<
-    'idle' | 'pending' | 'success' | 'error'
-  >('idle')
-  const { formik, status: loginStatus } = useForm({
+  const { formik, status } = useForm({
     url: 'http://localhost:3000/api/auth/login',
     initialValues: {
       username: '',
@@ -17,23 +13,11 @@ export const useLogin = () => {
     },
     withCredentials: true,
   })
-  const { getUserProfile, userProfileStatus } = useUserProfile()
-
   const navigate = useNavigate()
 
   useEffect(() => {
-    if (loginStatus === 'pending') setStatus('pending')
-  }, [loginStatus])
-
-  useEffect(() => {
-    if (loginStatus === 'success') getUserProfile()
-  }, [loginStatus, getUserProfile])
-
-  useEffect(() => {
-    if (userProfileStatus === 'success') {
-      navigate('/dashboard')
-    }
-  }, [navigate, userProfileStatus])
+    if (status === 'success') navigate('/dashboard')
+  }, [status, navigate])
 
   return { formik, status }
 }
