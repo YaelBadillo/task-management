601d6ce7589cc5c4ecbd463ad69b0fef5344f751
Yael Badillo <yael.badillos@gmail.com>
2023-07-27 00:55:48 -0600

    Feat: add app reducer dispatch function to the app context

--- apps/frontend/src/context/AppContext.tsx
+++ apps/frontend/src/context/AppContext.tsx
@@ -1,9 +1,12 @@
-import { createContext } from 'react'
+import { Dispatch, createContext } from 'react'
 
 import { UserDto } from 'shared'
 
+import { AppActions } from '@utils/app-reducer'
+
 interface IAppContext {
   userProfile?: UserDto
+  dispatch?: Dispatch<AppActions>
 }
 
-export const AppContext = createContext<IAppContext>({ userProfile: undefined })
+export const AppContext = createContext<IAppContext>({})
