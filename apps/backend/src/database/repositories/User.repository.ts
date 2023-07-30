a82949b69ee623e5aa53f536bb5f6e7d046da8ed
Yael Badillo <yael.badillos@gmail.com>
2023-07-29 23:02:16 -0600

    Feat: remove register interface from user repository

--- apps/backend/src/database/repositories/User.repository.ts
+++ apps/backend/src/database/repositories/User.repository.ts
@@ -5,15 +5,8 @@ import { MongooseRepository } from '@database/repositories/Mongoose.repository'
 import { IUser } from '@database/models'
 import { InternalServerErrorException } from '@shared/exceptions'
 
-interface IUserRepository {
-  register(newUser: IUser): Promise<IUser>
-}
-
 @Service()
-export class UserRepository
-  extends MongooseRepository<IUser>
-  implements IUserRepository
-{
+export class UserRepository extends MongooseRepository<IUser> {
   constructor(
     @Inject('user.model')
     protected readonly model: Model<IUser>,
