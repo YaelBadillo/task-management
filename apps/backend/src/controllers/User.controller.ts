abf15e5cfec76de54751086b92167e19fb18bbe3
Yael Badillo <yael.badillos@gmail.com>
2023-08-01 23:17:06 -0600

    Refactor: change request type

--- apps/backend/src/controllers/User.controller.ts
+++ apps/backend/src/controllers/User.controller.ts
@@ -2,11 +2,10 @@ import { Response } from 'express'
 import { Service } from 'typedi'
 
 import { UserModel } from '@database/models'
-import { RequestWithUser } from '@shared/interfaces/requests'
 
 @Service()
 export class UserController {
-  getUserProfile(req: RequestWithUser, res: Response) {
+  getUserProfile(req: Requests.WithUser, res: Response) {
     const user = UserModel.toDto(req.user)
     res.json(user)
   }
