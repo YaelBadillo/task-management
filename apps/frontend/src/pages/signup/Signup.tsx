718da5da36d728bb7c2e5741fa80cb030832eea2
Yael Badillo <yael.badillos@gmail.com>
2023-09-18 14:22:37 -0600

    Refactor: move strings as constants

--- apps/frontend/src/pages/signup/Signup.tsx
+++ apps/frontend/src/pages/signup/Signup.tsx
@@ -1,6 +1,12 @@
 import { AuthSwitch, Button, Form, Heading, Input } from '@components/auth-form'
 import { Loading } from '@components/loading'
 import { useSignup } from '@signup/useSignup'
+import {
+  SignupAuthSwitch,
+  SignupConfirmPasswordInput,
+  SignupPasswordInput,
+  SignupUsernameInput,
+} from '@signup/signup.const'
 
 export const Signup = () => {
   const { form, status } = useSignup()
@@ -14,27 +20,27 @@ export const Signup = () => {
       <Heading>Create your account</Heading>
       <Form handleSubmit={handleSubmit}>
         <Input
-          type="text"
-          placeholder="Username"
-          name="username"
+          type={SignupUsernameInput.type}
+          placeholder={SignupUsernameInput.placeholder}
+          name={SignupUsernameInput.name}
           value={username.value}
           error={username.error}
           onBlur={handleBlur}
           onChange={handleChange}
         />
         <Input
-          type="password"
-          placeholder="Password"
-          name="password"
+          type={SignupPasswordInput.type}
+          placeholder={SignupPasswordInput.placeholder}
+          name={SignupPasswordInput.name}
           value={password.value}
           error={password.error}
           onBlur={handleBlur}
           onChange={handleChange}
         />
         <Input
-          type="password"
-          placeholder="Confirm password"
-          name="passwordConfirm"
+          type={SignupConfirmPasswordInput.type}
+          placeholder={SignupConfirmPasswordInput.placeholder}
+          name={SignupConfirmPasswordInput.name}
           value={passwordConfirm.value}
           error={passwordConfirm.error}
           onBlur={handleBlur}
@@ -42,7 +48,10 @@ export const Signup = () => {
         />
         <Button disabled={hasError}>Sign up</Button>
       </Form>
-      <AuthSwitch linkText="Login" linkPath="/auth/login">
+      <AuthSwitch
+        linkText={SignupAuthSwitch.linkText}
+        linkPath={SignupAuthSwitch.linkPath}
+      >
         Already have an account?
       </AuthSwitch>
     </>
---
+++ apps/frontend/src/pages/signup/signup.const.ts
@@ -0,0 +1,32 @@
+import { Type } from '@components/auth-form'
+
+export const SIGNUP_URL = 'http://localhost:3000/api/auth/sign-up'
+
+interface Input {
+  type: Type
+  placeholder: string
+  name: string
+}
+
+export const SignupUsernameInput: Input = {
+  type: 'text',
+  placeholder: 'Username',
+  name: 'username',
+}
+
+export const SignupPasswordInput: Input = {
+  type: 'password',
+  placeholder: 'Password',
+  name: 'password',
+}
+
+export const SignupConfirmPasswordInput: Input = {
+  type: 'password',
+  placeholder: 'Confirm password',
+  name: 'confirmPassword',
+}
+
+export const SignupAuthSwitch = {
+  linkText: 'Login',
+  linkPath: '/auth/login',
+}
